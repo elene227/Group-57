bc70cd5cefd0c87f86c5f1aa6a22d1bad263d447
elene227 <elelopirtskhalava@gmail.com>
2025-09-11 20:55:48 +0400

    commit

---
+++ Day 98/homework/for2.js
@@ -0,0 +1,10 @@
+// 3)შექმენი 5 ცვლადი სადაც შეინახავ ნამბერ ტიპის მონაცემს მაგ:let num1 = 22 , შენი დავალებაა 5 ივე ცვლადში მყოფი ელემენტები გადააქციოთ string ტიპის მონაცემად და დააკონსოლლოგოთ 5 ივე მათგანი
+
+let int0 = 22
+let int1 = 23
+let int2 = 24
+let int3 = 25
+let int4 = 26
+
+
+console.log(String(int0), String(int1), String(int2), String(int3), String(int4))
