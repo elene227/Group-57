639312d167494594d39a6b92a30c7437417b194b
elene227 <elelopirtskhalava@gmail.com>
2026-01-26 16:17:14 +0400

    commit

---
+++ Day 133/homework/for1.js
@@ -0,0 +1,27 @@
+//7)დაწერე კოდი, სადაც არის:
+
+// ერთი global ცვლადი
+
+// ორი block scope ცვლადი (ერთი if ბლოკში და მეორე for ციკლში)
+
+// შენი დავალებაა
+
+// console.log–ით გამოიტანე ყველა ცვლადი ბლოკის გარეთ და ნახე შედეგი
+
+
+let g = "nia"
+
+if(true){
+    let b1 = "hello"
+    
+}
+
+for(let i = 1; i <= 10; i++){
+    let b2 = `${i} hi.`
+    
+}
+
+
+console.log(g)
+console.log(b1)
+console.log(b2)
